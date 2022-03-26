f4eef276e8fc9945b68c89b8f006ead100995330
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-03-25 20:12:20 -0400

    update nginx organization

---
+++ cluster/nginx-ingress/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+helm template ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace --set controller.metrics.enabled=true --set controller.metrics.serviceMonitor.enabled=true > base.yaml 
