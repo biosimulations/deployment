ad25dde47f2a26aa0e53c3a28508f206fa32ceb8
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-03-26 20:04:08 -0400

    debug metrics

--- cluster/nginx-ingress/compile.sh
+++ cluster/nginx-ingress/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-helm template ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace --set controller.metrics.enabled=true --set controller.metrics.serviceMonitor.enabled=true > base.yaml 
+helm template ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace --set controller.metrics.enabled=true --set controller.metrics.serviceMonitor.enabled=true --set controller.metrics.serviceMonitor.additionalLabels.release="monitoring"> base.yaml 
